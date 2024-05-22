ed8897ded5fdc778e4035119846306f88caee5ad
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2024-05-22 18:07:39 +0000

    .

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -3,22 +3,35 @@ import Button from '../../ui/Button'
 import CreateCabinForm from './CreateCabinForm'
 import Modal from '../../ui/Modal'
 
-const AddCabin = () => {
-    const [isOpenModal, setIsOpenModal] = useState(false)
-  return (
-    <div>
-         <Button onClick={()=>setIsOpenModal(showForm=>!showForm)}>
-        Add New Cabin
-      </Button>
-      {
-        isOpenModal &&
-      <Modal onClose={()=>setIsOpenModal(false)}>
-        <CreateCabinForm onCloseModal={()=>setIsOpenModal(false)}/>
-      </Modal>
-      }
+// const AddCabin = () => {
+//     const [isOpenModal, setIsOpenModal] = useState(false)
+//   return (
+//     <div>
+//          <Button onClick={()=>setIsOpenModal(showForm=>!showForm)}>
+//         Add New Cabin
+//       </Button>
+//       {
+//         isOpenModal &&
+//       <Modal onClose={()=>setIsOpenModal(false)}>
+//         <CreateCabinForm onCloseModal={()=>setIsOpenModal(false)}/>
+//       </Modal>
+//       }
 
-    </div>
-  )
+//     </div>
+//   )
+// }
+
+function AddCabin(){
+    
+     return(
+     <Modal>
+        <Modal.Open opens='cabin-form'>
+            <Button>Add New Cabin</Button>
+        </Modal.Open>
+        <Modal.Window name='cabin-form'>
+            <CreateCabinForm/>
+        </Modal.Window>
+     </Modal>)
 }
 
 export default AddCabin
