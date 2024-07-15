e4e67829395bf5be2bf5375e5479b3e55495ec4d
Addo Michael <michaeladdo1997@gmail.com>
2024-07-15 12:50:30 +0000

    Update useLogin.js

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -3,19 +3,19 @@ import { login } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
-export function useLogin(){
-    const queryClient = useQueryClient()
-    const navigate = useNavigate()
-    const {mutate:loginFn, isLoading} = useMutation({
-        mutationFn:({email, password})=>login({email, password}),
-        onSuccess:(user)=>{
-            queryClient.setQueryData(['user'], user.user)
-            navigate('/', {replace:true})
-        },
-        onError:(err)=>{
-            toast.error('The provided email or password are incorrect')
-        }
-    })
+export function useLogin() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutate: loginFn, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => login({ email, password }),
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/", { replace: true });
+    },
+    onError: (err) => {
+      toast.error("The provided email or password are incorrect");
+    },
+  });
 
-    return {loginFn, isLoading}
+  return { loginFn, isLoading };
 }
